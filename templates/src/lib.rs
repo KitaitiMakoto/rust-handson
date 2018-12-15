fa30c1626480302b9c349040ef3c8ec95c33a9a9
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2018-12-15 15:41:52 +0900

    Solve hands on problem

--- templates/src/lib.rs
+++ templates/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
 
 #[wasm_bindgen]
 extern "C" {
@@ -11,6 +13,14 @@ pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", to_hash_digest(name)));
 }
 
-fn to_hash_digest(s: &str) -> &str {
-    s
+fn to_hash_digest(s: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    let b = s.as_bytes();
+    hasher.write(b);
+    hasher.finish()
+}
+
+#[test]
+fn test_to_hash_digest() {
+    assert_eq!(to_hash_digest("World!"), 531153381967649867);
 }
